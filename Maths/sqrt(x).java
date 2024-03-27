6e74f5e66829ae32a58b1235c3b312851c9f4c84
AKHIL KUMAR <akhilkumar200485@gmail.com>
2024-03-27 22:53:24 +0530

    LC Ques 69

---
+++ Maths/sqrt(x).java
@@ -0,0 +1,11 @@
+public class sqrt(x){
+
+    public static void main(String[] args) {
+        System.out.println(sqrt(4));
+    }
+
+    static int sqrt(int x) {
+        int ans = (int) Math.sqrt(x);
+        return ans;
+    }
+}
