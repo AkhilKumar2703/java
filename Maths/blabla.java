a3eabd34108f0e8733d6445091ea01a89f77f2dd
AKHIL KUMAR <akhilkumar200485@gmail.com>
2024-03-27 22:53:11 +0530

    LC ques 69

--- Maths/blabla.java
+++ Maths/blabla.java
@@ -3,6 +3,8 @@ package Maths;
 public class blabla {
     public static void main(String[] args) {
         int n = 10;
-        System.out.println(Roman);
+        System.out.println("Roman");
+
     }
+
 }
