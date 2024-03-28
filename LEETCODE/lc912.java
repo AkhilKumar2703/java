0dd5ac575d7e9c7c09ceda283be869295fb5049f
AKHIL KUMAR <akhilkumar200485@gmail.com>
2024-03-28 23:41:08 +0530

    Given an array of integers nums, sort the array in ascending order and return it.

---
+++ LEETCODE/lc912.java
@@ -0,0 +1,54 @@
+https://leetcode.com/problems/sort-an-array/
+class Solution {
+    public int[] sortArray(int[] arr) {
+        return ms(arr);
+    }
+
+    static int[] ms(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+        int m = arr.length / 2;
+
+        int[] left = ms(Arrays.copyOfRange(arr, 0, m));
+        int[] right = ms(Arrays.copyOfRange(arr, m, arr.length));
+
+        return merge(left, right);
+
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int[] mix = new int[left.length + right.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+
+                mix[k] = left[i];
+                i++;
+            } else {
+                mix[k] = right[j];
+                j++;
+
+            }
+            k++;
+        }
+        // if the arrays have different length and one goes out of bounds then
+        while (i < left.length) {
+            mix[k] = left[i];
+            i++;
+            k++;
+
+        }
+        while (j < right.length) {
+            mix[k] = right[j];
+            j++;
+            k++;
+
+        }
+        return mix;
+
+    }
+
+}
---
+++ OOPS/class.java
@@ -0,0 +1,5 @@
+package OOPS;
+
+public class class {
+    // given a task to make a list of 5 students which include their name roll marks
+}
