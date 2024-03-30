89bdec0ff4bd32dd5010288551a88b442ee3d05d
AKHIL KUMAR <akhilkumar200485@gmail.com>
2024-03-30 23:52:20 +0530

    oops

--- OOPS/class.java
+++ OOPS/class.java
@@ -1,5 +1,16 @@
 package OOPS;
 
 public class class {
-    // given a task to make a list of 5 students which include their name roll marks
+    // given a task to make a list of 5 students which include their name, roll ,marks
+    int [] rollNo = new int[5];
+    String [] name = new String[5];
+    float [] marks = new float[5];
+    // in oops we create our owm datatype and store all the information in that
+    // we call it a CLASS
+    Students[] student = new Students[5];
+}
+class Students {
+    int [] rollNo = new int[5];
+    String [] name = new String[5];
+    float [] marks = new float[5];
 }
