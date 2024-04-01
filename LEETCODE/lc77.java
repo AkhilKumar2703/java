6fe09a904e39d30d099968ffd76e91e05a04f2c2
AKHIL KUMAR <akhilkumar200485@gmail.com>
2024-04-01 21:11:38 +0530

    cobination ques 77

---
+++ LEETCODE/lc77.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> subset = new ArrayList<Integer>();
+        combi(1, n, new ArrayList<>(), subset, k);
+        return subset;
+    }
+
+    static void combi(int start, int n, List<Integer> current, List<List<Integer>> subset, int k) {
+        // base condition for k
+        if (current.size() == k) {
+             subset.add(new ArrayList<>(current)):
+             return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            // subset
+            current.add(i);
+            combin(start + 1, n, current, subset, k);
+            // backtrak for new subset of different n values
+            // basically the current list will get empty
+            current.remove(current.size() - 1);
+        }
+    }
+}
